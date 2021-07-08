ba026921fab9c03d15ee68c34bcd0c44929b0a1c
Than Htut Aung <rday61062@gmail.com>
2021-07-08 17:45:41 +0630

    netli test

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,5 @@
 const NotFound = () => {
-    return <h3 className="text-center pt-5">404 not found</h3>;
+    return <h3 className="text-center pt-5">404 not found. Please enter correct URL</h3>;
 };
 
 export default NotFound;
